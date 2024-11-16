06a0c47976d5c115dff5c983ea8889866d4dec40
Kyalo460 <kyalo460@gmail.com>
2024-11-16 08:19:33 +0300

    used a different icon

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
         <h3>Email: Kyalo460@gmail.com</h3>
         <div className="links-wrapper">
           <a href="https://github.com/Kyalo460" target="_blank" rel="noopener noreferrer"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
-          <a href="https://www.linkedin.com/in/brian-kyalo-17a438243" target="_blank"  rel="noopener noreferrer"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/in/brian-kyalo-17a438243" target="_blank"  rel="noopener noreferrer"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
