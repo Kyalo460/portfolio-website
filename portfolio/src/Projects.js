f26d18d1e95032c428a0ebd609b90ad1e0589ef0
Kyalo460 <kyalo460@gmail.com>
2024-11-21 10:27:03 +0300

    added link to smatgarage

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -4,11 +4,13 @@ export default function Projects() {
       <h2 className="projects-header">I am currently working on deploying the projects</h2>
       <div className="projects-wrapper">
         <div>
-          <h3><a href="https://github.com/Kyalo460/Smartgarage" target="_blank" rel="noreferrer">Smartgarage</a></h3>
+          <h4><a href="https://smartgarage-phi.vercel.app/" target="_blank" rel="noreferrer">Smartgarage</a></h4>
+          <h4><a href="https://github.com/Kyalo460/Smartgarage" target="_blank" rel="noreferrer">Source Code</a></h4>
           <img className="website" src={`${process.env.PUBLIC_URL}/website1.png`} alt="website_image" />
         </div>
         <div>
-          <h3><a href="https://github.com/Kyalo460/varsity_campus" target="_blank" rel="noreferrer">Varsity_campus</a></h3>
+          <h4><a href="">Varsity_campus</a></h4>
+          <h4><a href="https://github.com/Kyalo460/varsity_campus" target="_blank" rel="noreferrer">Source Code</a></h4>
           <img className="website" src={`${process.env.PUBLIC_URL}/website2.png`} alt="website_image" />
         </div>
       </div>
