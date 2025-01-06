db3c042680154d6fa546e513e51b598dfb82feba
Kyalo460 <kyalo460@gmail.com>
2025-01-06 05:42:20 +0300

    added active class

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './Home.js'
-import About from './About.js'
-import Resume from './Resume.js';
-import Projects from './Projects.js';
-import Contact from './Contact.js';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import Home from "./Home.js";
+import About from "./About.js";
+import Resume from "./Resume.js";
+import Projects from "./Projects.js";
+import Contact from "./Contact.js";
+import { useState, useEffect } from "react";
 
 function App() {
   const [isHidden, setIsHidden] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(window.scrollY);
 
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,32 +23,69 @@ function App() {
         setIsHidden(false);
       }
       setLastScrollY(window.scrollY);
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [lastScrollY]);
 
   return (
     <div className="App">
-      <nav className={`navbar ${isHidden ? 'hidden' : ''}`}>
-        <ul className='nav-container'>
-          <li className='nav-items'><Link to='/portfolio-website/'>Home</Link></li>
-          <li className='nav-items'><Link to='/portfolio-website/about'>About</Link></li>
-          <li className='nav-items'><Link to='/portfolio-website/resume'>Resume</Link></li>
-          <li className='nav-items'><Link to='/portfolio-website/projects'>Projects</Link></li>
-          <li className='nav-items'><Link to='/portfolio-website/contact'>Contact</Link></li>
+      <nav className={`navbar ${isHidden ? "hidden" : ""}`}>
+        <ul className="nav-container">
+          <li className="nav-items">
+            <Link
+              to="/portfolio-website/"
+              className={isActive("/portfolio-website/") ? "active" : ""}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link
+              to="/portfolio-website/about"
+              className={isActive("/portfolio-website/about") ? "active" : ""}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link
+              to="/portfolio-website/resume"
+              className={isActive("/portfolio-website/resume") ? "active" : ""}
+            >
+              Resume
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link
+              to="/portfolio-website/projects"
+              className={
+                isActive("/portfolio-website/projects") ? "active" : ""
+              }
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-items">
+            <Link
+              to="/portfolio-website/contact"
+              className={isActive("/portfolio-website/contact") ? "active" : ""}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
       <Routes>
-        <Route path='/portfolio-website/' element={<Home />} />
-        <Route path='/portfolio-website/about' element={<About />} />
-        <Route path='/portfolio-website/resume' element={<Resume />} />
-        <Route path='/portfolio-website/projects' element={<Projects />} />
-        <Route path='/portfolio-website/contact' element={<Contact />} />
+        <Route path="/portfolio-website/" element={<Home />} />
+        <Route path="/portfolio-website/about" element={<About />} />
+        <Route path="/portfolio-website/resume" element={<Resume />} />
+        <Route path="/portfolio-website/projects" element={<Projects />} />
+        <Route path="/portfolio-website/contact" element={<Contact />} />
       </Routes>
     </div>
   );
